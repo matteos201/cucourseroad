275d77460553112388793de16c2ad1788f1b8f63
Matthew H <matthewgy10@gmail.com>
2024-06-16 11:06:52 -0600

    Moved essential program functionality to main funciton. Runs repeatedly. Added confirmation checks. Now allows to add classes on different sessions. Allows save to file and showing progress.

--- webscraping.py
+++ webscraping.py
@@ -55,40 +55,56 @@ def semester_and_year():
 
 def course_selection(courses_catalog):
     my_courses = []
-    while True:
-        user_input = input("Please insert course number (enter 'done' to finish adding courses):\n> ")
-        if user_input == 'done':
-            break
-        elif user_input in courses_catalog:
-            while True:
-                confirm = input("Confirm to add this course? (y/n):\n> ").lower()
-                if confirm == 'y':
-                    print(f"Added {courses_catalog[user_input]}")
-                    my_courses.append(courses_catalog[user_input])
-                    break
-                elif confirm == 'n':
-                    break
-                else:
-                    print("Please enter n for no or y for yes")
 
-        else:
-            print("Cannot identify course number")
+    while True:
+        add_or_del = input("Add or delete a course? (add/del):\n> ").lower()
+        match add_or_del:
+            case 'add':
+                while True:
+                    course_input = input("Please insert course number (enter 'done' to finish adding courses):\n> ")
+                    if course_input == 'done':
+                        break
+                    elif course_input in courses_catalog:
+                        while True:
+                            confirm = input("Confirm to add this course? (y/n):\n> ").lower()
+                            if confirm == 'y':
+                                print(f"Added {courses_catalog[course_input]}")
+                                courses_catalog[course_input] = courses_catalog[course_input].replace('\xa0', '')
+                                my_courses.append(courses_catalog[course_input])
+                                break
+                            elif confirm == 'n':
+                                break
+                            else:
+                                print("Please enter n for no or y for yes")
+
+                    else:
+                        print("Cannot identify course number")
+                break
+            case 'del':
+                while True:
+                    del_input = input("Enter the id of the course you'd like to delete. (enter 'done' to finish removing courses):\n> ")
+                    if del_input == 'done':
+                        break
+                    elif del_input in my_courses:
+                        while True:
+                            confirm = input("Confirm to delete this course? (y/n):\n> ").lower()
+                            if confirm == 'y':
+                                print(f"Deleted {my_courses[del_input]}")
+                                del (my_courses[del_input])
+                                break
+                            elif confirm == 'n':
+                                break
+                            else:
+                                print("Please enter n for no or y for yes")
+                    else:
+                        print("Course number not in your courses list")
+
+                break
+            case _:
+                print("Please enter add or del")
     return my_courses
 
 def main():
-    while True:
-        
-        choices = input("\nWhat would you like to do?\n> ")
-        match choices:
-            case "plan":
-                sem_and_year = semester_and_year()
-                print(f'Your semester and year: {sem_and_year}')
-                break
-            case "quit":
-                exit()
-            case _:
-                print("Sorry. Your input does not match our choices.")
-                continue
     avail_depts = webscrape_depts()
     while True:
         confirm = ''
@@ -109,37 +125,58 @@ def main():
         else:
             print("This is not a valid department code!")
         if confirm == 'y':
-            break
+            break    
+    return my_courses
+    
+    
+    
+    
+    
+    
 
-    print(f"Courses picked for {sem_and_year[0]}, {sem_and_year[1]}:")
-    for course in my_courses:
-        print(course)
-    files = []
-    while True:
-        save_to_file = input("Save to file? (y/n)\n> ").lower()
-        match save_to_file:
-            case 'y':
-                file_name = f"{sem_and_year[0]}{sem_and_year[1]}.txt"
-                f = open(file_name, "w")
-                files.append(file_name)
-                f.write(f"Courses picked for {sem_and_year[0]}, {sem_and_year[1]}:")
-                for course in my_courses:
-                    f.write(f"\n{course}")
-                f.close()
-                break
-            case 'n':
-                break
-            case 'quit':
-                exit()
-            case _:
-                print("Please enter y for yes or n for no.")
-                continue
-
-    main()
 
 if __name__ == "__main__":
     print("\n\nWELCOME TO COURSEROAD FOR CU!\n----------------------------\n")
     print("--------Controls Guide--------")
     print("'quit': Quits program")
     print("'plan': Start planning semester")
-    main()
+    print("'show': Displays course list by semester and year")
+    print("'save': Saves course list to txt file")
+
+    courses_dict = {}
+    sem_and_year = ()
+    while True:
+        while True:
+            choices = input("\nWhat would you like to do?\n> ")
+            match choices:
+                case "plan":
+                    sem_and_year = semester_and_year()
+                    print(f'Your semester and year: {sem_and_year}')
+                    break
+                case "quit":
+                    exit()
+                case "show":
+                    print(courses_dict)
+                case "save":
+                    while True:
+                        save_to_file = input("Save to file? (y/n)\n> ").lower()
+                        match save_to_file:
+                            case 'y':
+                                file_name = "myCourseList.txt"
+                                f = open(file_name, "w")
+                                f.write(str(courses_dict))
+                                f.close()
+                                break
+                            case 'n':
+                                break
+                            case 'quit':
+                                exit()
+                            case _:
+                                print("Please enter y for yes or n for no.")
+                                continue
+                case _:
+                    print("Sorry. Your input does not match our choices.")
+        if sem_and_year not in courses_dict:
+            courses_dict[(sem_and_year[0], sem_and_year[1])] = []
+        courses_dict[(sem_and_year[0], sem_and_year[1])] += main()
+        print(courses_dict)
